65a6baea470b1aa4dfc8a5fe723ab64a69ddab69
LastLosts <artem.cppdev@gmail.com>
2025-04-15 19:42:46 +0300

    первый эпизод

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
